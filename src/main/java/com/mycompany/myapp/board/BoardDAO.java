f2f7fd2bbda9eee8ad5f31fc8367a3bef77b41f1
양시준 <sijun0905@gmail.com>
2022-12-06 18:45:01 +0900

    apply mybatis

--- src/main/java/com/mycompany/myapp/board/BoardDAO.java
+++ src/main/java/com/mycompany/myapp/board/BoardDAO.java
@@ -1,5 +1,6 @@
 package com.mycompany.myapp.board;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,11 +17,7 @@ import java.util.List;
 public class BoardDAO {
 
     @Autowired
-    JdbcTemplate template;
-
-    public void setTemplate(JdbcTemplate template) {
-        this.template = template;
-    }
+    SqlSession sqlSession;
 
     Connection conn = null;
     PreparedStatement stmt = null;
@@ -33,34 +30,18 @@ public class BoardDAO {
     private final String BOARD_LIST = "select * from Board order by seq desc";
 
     public int insertBoard(BoardVO vo) {
-        return template.update(BOARD_INSERT, vo.getTitle(), vo.getWriter(), vo.getContent());
+        return sqlSession.insert("Board.insertBoard", vo);
     }
     public int deleteBoard(int seq) {
-        return template.update(BOARD_DELETE, seq);
+        return sqlSession.delete("Board.deleteBoard", seq);
     }
     public int updateBoard(BoardVO vo) {
-        return template.update(BOARD_UPDATE, vo.getTitle(), vo.getWriter(), vo.getContent(), vo.getSeq());
+        return sqlSession.update("Board.updateBoard", vo);
     }
     public BoardVO getBoard(int seq) {
-        return template.queryForObject(BOARD_GET, new Object[] {seq}, new BeanPropertyRowMapper<BoardVO>(BoardVO.class));
+        return sqlSession.selectOne("Board.getBoard", seq);
     }
     public List<BoardVO> getBoardList() {
-        return template.query(BOARD_LIST, new RowMapper<BoardVO>() {
-            @Override
-            public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-                BoardVO data = new BoardVO();
-                data.setSeq(rs.getInt("seq"));
-                data.setTitle(rs.getString("title"));
-                data.setWriter(rs.getString("writer"));
-                data.setContent(rs.getString("content"));
-                data.setRegDate(rs.getTimestamp("regDate").toLocalDateTime());
-                System.out.println(data.getSeq());
-                System.out.println(data.getTitle());
-                System.out.println(data.getWriter());
-                System.out.println(data.getContent());
-                System.out.println(data.getRegDate());
-                return data;
-            }
-        });
+        return sqlSession.selectList("Board.getBoardList");
     }
 }
